07db0c79fbf0220d4fbf3fad08b305e0788f289a
Will Haley <willhaley@boddienoell.com>
2016-04-07 13:45:47 -0400

    updates to the system

--- get_data.sh
+++ get_data.sh
@@ -2,6 +2,10 @@
 
 rm /var/data/tides.json
 
-wget -O /var/data/tides.json 'https://www.worldtides.info/api?extremes&lat=35.595438&lon=-77.585250&key=6f535570-09f1-4aac-88bd-48d5dbb3e124'
+wget -O /var/data/tides.json 'https://www.worldtides.info/api?extremes&lat=34.3538323&lon=-77.6428886&key=6f535570-09f1-4aac-88bd-48d5dbb3e124'
 
-echo "var tides = "|cat - /var/data/tides.json > /tmp/out && mv /tmp/out /home/pi/projects/beach-app/dist/tides.js
+echo "var tides = "|cat - /var/data/tides.json > /tmp/out && mv /tmp/out /home/will/projects/tides-app/src/tides.js
+
+wget -O /var/data/weather.json 'https://api.forecast.io/forecast/4b087fe12683c3b471c85c74499920d5/34.3538323,-77.6428886'
+
+echo "var weather = "|cat - /var/data/weather.json > /tmp/out && mv /tmp/out /home/will/projects/tides-app/src/weather.js
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -12,6 +12,7 @@
     var dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     main.tides = _.chunk(tides.extremes, 5);
     main.weather = _.chunk(weather.daily.data, 7);
-    console.log( main.weather );
+    main.current = weather.currently;
+    console.info( 'weather', main.current );
   }
 })();
