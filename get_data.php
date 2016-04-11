8684d0b0cb60ca52c7c2872233ad31fb65839373
Will Haley <willhaley@boddienoell.com>
2016-04-11 16:03:48 -0400

    moving the get data script back to production

--- get_data.php
+++ get_data.php
@@ -2,13 +2,13 @@
 
 	if ( $tides = @file_get_contents( 'https://www.worldtides.info/api?extremes&lat=34.3538323&lon=-77.6428886&key=6f535570-09f1-4aac-88bd-48d5dbb3e124' ) ){
 		$tides = 'var tides = ' . $tides;
-		file_put_contents( '/home/will/projects/tides-app/src/tides.js', $tides );
+		file_put_contents( '/home/pi/projects/beach-app/dist/tides.js', $tides );
 	
 	}
 	
 
 	if ( $weather = @file_get_contents( 'https://api.forecast.io/forecast/4b087fe12683c3b471c85c74499920d5/34.3538323,-77.6428886' ) ){
 		$weather = 'var weather = ' . $weather;
-		file_put_contents( '/home/will/projects/tides-app/src/weather.js', $weather );
+		file_put_contents( '/home/pi/projects/beach-app/dist/weather.js', $weather );
 	
 	}
